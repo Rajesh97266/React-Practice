6b43ddf33c77a6fe1111ae642dcdf0721881be68
Rajesh97266 <rajesh97266@gmail.com>
2024-11-28 15:57:52 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <textarea
+        readOnly
+        maxLength={3}
+        spellCheck
+        style={{ backgroundColor: "gray" }}
+      />
+      <h1>HI</h1>
+    </div>
+  );
+}
+
+export default App
