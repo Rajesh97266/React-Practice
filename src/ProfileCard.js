a7e1823f5bb893010cd4a2bec556b795e01aaa6a
Rajesh97266 <rajesh97266@gmail.com>
2024-11-28 22:33:35 +0530

    fourth

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,22 @@
-function ProfileCard(props){
+function ProfileCard({ title, handle }) {
+  // const title = props.title;
+  // const handle = props.handle;
+  // const {title , handle} = props;
   return (
     <div>
-      <div>This is {props.title}</div>
-      <div>This is {props.handle}</div>
+      <div>This is {title}</div>
+      <div>This is {handle}</div>
+      <div>This is {title}</div>
+      <div>This is {handle}</div>
+      <div>This is {title}</div>
+      <div>This is {handle}</div>
+
+      <div>This is {title}</div>
+      <div>This is {handle}</div>
+      <div>This is {title}</div>
+      <div>This is {handle}</div>
     </div>
   );
 }
 
-export default ProfileCard
+export default ProfileCard;
