88fee488240b64c001894a2ee71b86ad75b9a965
Rajesh97266 <rajesh97266@gmail.com>
2024-11-29 11:23:23 +0530

    sixth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png"
+import CortonaImage from "./images/cortana.png"
+import SiriImage from "./images/siri.png"
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cotana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <ProfileCard
+        title="Alexa"
+        handle="@alexa99"
+        imageSrc={AlexaImage}
+        alt="alexa"
+      />
+      <ProfileCard
+        title="Cortana"
+        handle="@cotana32"
+        imageSrc={CortonaImage}
+        alt="cortana"
+      />
+      <ProfileCard
+        title="Siri"
+        handle="@siri01"
+        imageSrc={SiriImage}
+        alt="siri"
+      />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,11 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle , imageSrc , alt}) {
   // const title = props.title;
   // const handle = props.handle;
   // const {title , handle} = props;
+  // console.log(title,handle);
   return (
     <div>
+      <img src={imageSrc} alt={alt}/>
       <div>This is {title}</div>
       <div>This is {handle}</div>
       
